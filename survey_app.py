f5396d0e1dde8eeae89800c1fd0fc664829fcb92
liambarnett <liamwba@hotmail.com>
2017-03-29 15:08:19 +0100

    update

--- survey_app.py
+++ survey_app.py
@@ -160,10 +160,10 @@ def makeWebhookResult(req):
         "source": "apiai-pulse-survey"
     }
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    app.run(HOST, PORT)'''
